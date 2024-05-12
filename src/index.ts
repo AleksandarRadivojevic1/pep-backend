d2b1727d1601a7b4faa6e9c1e584b90104b5ad36
Aleksandar Radivojevic <radivojevicaleksandar64@gmail.com>
2024-05-12 15:15:57 +0200

    Podesavanje osnova ExpressJS-a

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-console.log('Hello world!')
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express ()
+app.use(express.json())
+app.use(cors())
+app.use(morgan('tiny'))
+
+
+
+const port = 4000
+
+app.listen(port, ()=>{
+    console.log("App started and listening on " + port)
+})
+
+
+app.get('/', (req , res) => {
+    res.json({
+        message: "Hello world from ExpressJs and Typescript!"
+    })
+})
+
+app.get('*', (req , res) => {
+    res.status(404).json({
+        message: "Not found!"
+    })
+})
