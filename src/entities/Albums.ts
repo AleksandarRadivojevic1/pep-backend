8bb59bf9f170228c97837bd2506cb92e30194954
Aleksandar Radivojevic <radivojevicaleksandar64@gmail.com>
2024-06-12 15:00:02 +0200

    Dodata konekcija ka bazi podataka

---
+++ src/entities/Albums.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { Artists } from "./Artists";
+import { Reviews } from "./Reviews";
+import { Songs } from "./Songs";
+
+@Index("fk_albums_artist_idx", ["artistId"], {})
+@Entity("albums", { schema: "praktikum_esistemi_db" })
+export class Albums {
+  @PrimaryGeneratedColumn({ type: "int", name: "album_id", unsigned: true })
+  albumId: number;
+
+  @Column("varchar", { name: "album_name", length: 255 })
+  albumName: string;
+
+  @Column("int", { name: "artist_id", unsigned: true })
+  artistId: number;
+
+  @ManyToOne(() => Artists, (artists) => artists.albums, {
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+  })
+  @JoinColumn([{ name: "artist_id", referencedColumnName: "artistId" }])
+  artist: Artists;
+
+  @OneToMany(() => Reviews, (reviews) => reviews.album)
+  reviews: Reviews[];
+
+  @OneToMany(() => Songs, (songs) => songs.album)
+  songs: Songs[];
+}
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import { configDotenv } from 'dotenv';
+import { AppDataSource } from './db';
 
-const app = express ()
+const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
 
 
+configDotenv()
+AppDataSource.initialize().then(() => {
+    console.log("Connected to Database!")
+    const port = process.env.SERVER_PORT || 4000
 
-const port = 4000
+    app.listen(port, () => {
+        console.log("App started and listening on " + port)
+    })
+}).catch((e) => console.log(e))
 
-app.listen(port, ()=>{
-    console.log("App started and listening on " + port)
-})
 
 
-app.get('/', (req , res) => {
+app.get('/', (req, res) => {
     res.json({
         message: "Hello world from ExpressJs and Typescript!"
     })
 })
 
-app.get('*', (req , res) => {
+app.get('*', (req, res) => {
     res.status(404).json({
         message: "Not found!"
     })
